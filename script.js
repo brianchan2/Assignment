8f9989f063fd0b95f642cd3f38b0ec1881ce1ac5
brianchan9061@gmail.com <brianchan9061@gmail.com>
2023-03-29 23:25:51 -0400

    Added choices and now clears movie data on new selection

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const movieInfo = document.querySelector("#movie")
+let movieInfo = document.querySelector("#movie")
 const API_KEY = "41d720cb66a9e8c5a0107c33dc1b4f32"
+let movieid = 550
+
+let select = document.querySelector("select")
 
 async function getTrailers(info) {
     if (info.videos) {
 
     }
 }
-
+async function deletediv(){
+    movieInfo.remove();
+    movieInfo = document.createElement("div");
+    document.body.append(movieInfo)
+}
 async function createDetails() {
     const items = {
         title: document.createElement("h2"),
@@ -24,38 +31,47 @@ async function createDetails() {
         movieInfo.appendChild(items[item])
     }
 
-    const info = (await axios({
+    let info = (await axios({
         method: "GET",
-        url: `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`,
+        url: `https://api.themoviedb.org/3/movie/${movieid}`,
+        params: {
+            api_key: API_KEY
+        }
     }).catch(err => {
         console.log(err)
-    })).data
+    }))
 
-    items["budget"].innerHTML = `Budget <br> <br>\$${info.budget}`
-    items["title"].innerHTML = `${info.title}`
-    items["description"].innerHTML = `${info.overview}`
-    items["popularity"].innerHTML = `Popularity <br> <br> ${Math.floor(info.popularity)} / 100`
-    items["release"].innerHTML = `Released <br> <br> ${info.release_date}`
-    items["duration"].innerHTML = `Duration <br> <br> ${info.runtime} mins`
+    if (info) {
+        info = info.data 
+        items["budget"].innerHTML = `Budget <br> <br>\$${info.budget}`
+        items["title"].innerHTML = `${info.title}`
+        items["description"].innerHTML = `${info.overview}`
+        items["popularity"].innerHTML = `Popularity <br> <br> ${Math.floor(info.popularity)} / 100`
+        items["release"].innerHTML = `Released <br> <br> ${info.release_date}`
+        items["duration"].innerHTML = `Duration <br> <br> ${info.runtime} mins`
 
-    const title = document.createElement("h2")
-    title.innerText = "Genres"
-    items["genres"].appendChild(title)
+        const title = document.createElement("h2")
+        title.innerText = "Genres"
+        items["genres"].appendChild(title)
 
-    if (info && info.genres) {
-        for (let tag of info.genres) {
-            let element = document.createElement("h5")
-            element.innerText = tag.name
-            element.className = "tag"
-            items["genres"].appendChild(element)
-            console.log("Created")
+        if (info && info.genres) {
+            for (let tag of info.genres) {
+                let element = document.createElement("h5")
+                element.innerText = tag.name
+                element.className = "tag"
+                items["genres"].appendChild(element)
+                console.log("Created")
+            }
         }
+
+        console.log(info)    
+
+        getTrailers(info)
     }
-
-    console.log(info)    
-
-    getTrailers(info)
 }
 
-
-createDetails()
+select.addEventListener("change", () => {
+    movieid = select.value
+    deletediv()
+    createDetails()
+})
