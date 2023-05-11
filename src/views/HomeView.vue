80dd7eae45a9a999ce8f2f7a34d049a0b62616b1
brianchan2 <brianchan9061@gmail.com>
2023-05-11 14:36:57 -0400

    Added buttons to the Hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,9 +7,15 @@
     <div id="hero">
         <div id="content">
             <h1>Enjoy Your Movies Anytime</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, asperiores?</p>
+            <p id="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, asperiores?</p>
+            <button>Browse</button>
+            <a href="./Login"><p id="prompt">New to the site? Create a new account!</p></a>
         </div>
     </div>
+    <footer>
+        <h1>Random Footer text</h1>
+    </footer>
+    
 </template>
 
 <style scoped>
@@ -19,27 +25,60 @@
         height: 100vw;
         width: 100%;
     }
+    footer {
+        position: fixed;
+        width: 100%;
+        text-align: center;
+        display: flex;
+        background-color: rgb(0, 0, 0);
+        bottom: 0;
+    }
     h1 {
         display: inline;
         font-size: 2.3rem;
         font-weight: bold;
         font-family: "helvetica"
     }
-    p {
+    #hero #description {
         font-size: 1rem;
         font-weight: bold;
-        font-family: "helvetica"
+        font-family: "helvetica";
+    }
+    #hero button {
+        border-style: none;
+        margin-top: 15px;
+        width: 10rem;
+        height: 1.2rem;
+        border-radius: 5px;
+        color: black;
+        font-weight: bold;
+        background-color: white;
+    }
+
+    #hero button:hover {
+        background-color: grey;
+    }
+
+    #hero #prompt {
+        margin-top: 15px;
+        font-size: 11px;
+        font-weight: lighter;
+        text-decoration: underline;
+    }
+
+    #hero #prompt:active {
+        color: grey;
     }
 
     #content {
-        margin-top: 2rem;
-        margin-left: 1%;
+        padding-top: 2rem;
+        padding-left: 1.5%;
         height: 50rem;
     }
 
     @media (max-width: 1000px) {
         h1 {
-            font-size: 4.6vw;
+            font-size: 3.6vw;
         }
         p {
             font-size: 2.6vw;
