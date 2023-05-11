4bd2372619b0d167b18b00314779d7b971429d01
brianchan9061@gmail.com <brianchan9061@gmail.com>
2023-05-10 23:35:53 -0400

    Added Home page

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <img src="https://www.jobalign.com/wp-content/uploads/2017/07/Icon-Placeholder-1.png">
+        <button>Login</button>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        width: 100%;
+        height: 3rem;
+        background-color: rgb(50,50,50);
+    }
+
+    img {
+        max-width: 80%;
+        max-height: 80%;
+        margin-left: 1rem;
+    }
+
+    button {
+        margin-left: auto;
+        margin-right: 1rem;
+        background-color: black;
+        border-style: none;
+        height: 2rem;
+        width: 4rem;
+        border-radius: 0.5rem;
+        font-weight: bold;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,58 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+    import Header from "../components/Header.vue"
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <Header />
+    <div id="hero">
+        <div id="content">
+            <h1>Enjoy Your Movies Anytime</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, asperiores?</p>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+    #hero {
+        background-color: black;
+        max-height: 20rem;
+        height: 100vw;
+        width: 100%;
+    }
+    h1 {
+        display: inline;
+        font-size: 2.3rem;
+        font-weight: bold;
+        font-family: "helvetica"
+    }
+    p {
+        font-size: 1rem;
+        font-weight: bold;
+        font-family: "helvetica"
+    }
+
+    #content {
+        margin-top: 2rem;
+        margin-left: 1%;
+        height: 50rem;
+    }
+
+    @media (max-width: 1000px) {
+        h1 {
+            font-size: 4.6vw;
+        }
+        p {
+            font-size: 2.6vw;
+        }
+    }
+
+    @media (max-width: 425px) {
+        h1 {
+            font-size: 2rem;
+        }
+        p {
+            font-size: 1rem;
+        }
+    }
+    
+</style>
