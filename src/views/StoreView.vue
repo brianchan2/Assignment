f376f642d1ef05f9b0b617e034fb7967264b80a7
brianchan2 <brianchan9061@gmail.com>
2023-05-12 13:49:35 -0400

    Added movie tiles

--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,23 +1,69 @@
-<script setup>
+<script async setup>
     import axios from "axios"
     import { useMovieStore } from "../stores/movie.js"
 
-    const moviedata = useMovieStore()
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const movieData = useMovieStore()
 
-    moviedata.movies.push("Ji")
-
-    console.log(moviedata.movies)
-
-    if (!moviedata.movies || !(moviedata.movies.length < 0)) {
-        console.log("You are missing movie data")
+    if (!movieData.movies || !(movieData.movies.length < 0)) {
+        console.log("Runnning")
+        axios({
+            url: "https://api.themoviedb.org/3/trending/movie/week",
+            method: "GET",
+            params: {
+                api_key: API_KEY,
+            }
+        }).then(movies => {
+            movies.data.results.forEach(movie => {
+            movieData.movies.push({
+                    id: movie.id,
+                    poster: movie.poster_path,
+                    title: movie.title
+                })
+            })
+        }).catch(err => {
+            console.log(err)
+        })
     }
+
+    console.log(movieData.movies) 
 
 </script>
 
 <template>
-
+    <h1>Popular</h1>
+    <div id="movies" v-if="movieData.movies">
+        <div id="movie" v-for="movie in movieData.movies">
+            <img :src="`https://image.tmdb.org/t/p/original${movie.poster}`" />
+            <h1>{{ movie.title }}</h1>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    #movies {
+        display: flex;
+        flex-direction: row;
+        gap: 1.5vw;
+        flex-wrap: wrap;
+    }
+    #movie {
+        width: 166px;
+    }
 
+    #movie h1 {
+        color: white;
+        font-size: 0.7em;
+        width: 100%;
+        text-overflow: clip;
+        text-align: center;
+        position: relative;
+        bottom: 10%;
+        height: 10%;
+        background-color: rgba(0, 0, 0, 0.7);
+    }
+
+    img {
+        width: 10rem
+    }
 </style>
